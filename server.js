739d3e207f42c0a3abe8057e3e02457d01759106
hsun1001208-eng <hsun1001208@gmail.com>
2026-09-06 19:29:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express=require("express");
 const OpenAI=require("openai");
 const app=express();
 app.use(express.json({limit:"12mb"}));
-app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
 app.post("/api/diary",async(req,res)=>{
   try{
